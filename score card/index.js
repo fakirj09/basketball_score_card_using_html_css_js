41116359874aa4f431fe5e5b77402869cabc73b7
fakirj09 <104077731+fakirj09@users.noreply.github.com>
2022-07-16 20:34:05 +0530

    Add files via upload

---
+++ score card/index.js
@@ -0,0 +1,30 @@
+let scoreEl = document.getElementById('score-el')
+let scoreEl2 = document.getElementById('score-el2')
+
+let team1 = 0
+
+function add1pointt1() {
+  team1 += 1
+  scoreEl.textContent = team1
+}
+function add2pointt1() {
+  team1 += 2
+  scoreEl.textContent = team1
+}
+function add3pointt1() {
+  team1 += 3
+  scoreEl.textContent = team1
+}
+let team2 = 0
+function add1pointt2() {
+  team2 += 1
+  scoreEl2.textContent = team2
+}
+function add2pointt2() {
+  team2 += 2
+  scoreEl2.textContent = team2
+}
+function add3pointt2() {
+  team2 += 3
+  scoreEl2.textContent = team2
+}
